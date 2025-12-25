40a6842a1025403788f8698e7e1797a06bad8936
theaterkid6or7 <loganjulien1313@gmail.com>
2025-12-25 10:04:21 -0600

    Implement player management functions

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
+// Basic shared state
+if (!localStorage.getItem("players")) {
+  localStorage.setItem("players", JSON.stringify([]));
+}
 
+function getPlayers() {
+  return JSON.parse(localStorage.getItem("players"));
+}
+
+function savePlayers(players) {
+  localStorage.setItem("players", JSON.stringify(players));
+}
+
+function addPlayer(name) {
+  const players = getPlayers();
+  players.push({
+    name,
+    score: 0,
+    sand: 0
+  });
+  savePlayers(players);
+}
